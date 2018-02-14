1be1b8153e0a6c961d7c65e1e9ee44e971ce0684
Ferril <ferril.darkdiver@gmail.com>
2018-02-14 03:56:04 +0300

    gunicorn app and nginx.conf fix

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 def app(environ, start_response):
 	status = '200 OK'
 	start_response(status, [('Content-Type', 'text/plain')])
-	q_strinq = environ['QUERY_STRING'].split('&')
-	resp = ''
-	for x in q_string:
-		resp += x + '\n'
-	return resp
+	data ='\r\n'.join(environ['QUERY_STRING'].split('&'))
+	return data
